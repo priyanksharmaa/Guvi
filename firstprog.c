3f0e3018fa55b9fc34ac31c4253ec2eef0115b56
priyanksharmaa <35268463+priyanksharmaa@users.noreply.github.com>
2018-01-09 22:58:46 +0530

    a

---
+++ firstprog.c
@@ -0,0 +1,12 @@
+#include<sdtio.h>
+void main(){
+int n,i;
+printf("Enter the no ");
+scanf("%d",&n);
+if(n>0)
+  printf("Positive");
+elseif (n=0)
+  printf("Zero");
+else
+  printf("Negative");
+}
