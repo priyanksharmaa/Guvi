b1e3d29bcf1dc0d99d4dd6faea2d1176eb36de24
priyanksharmaa <35268463+priyanksharmaa@users.noreply.github.com>
2018-01-11 22:10:14 +0530

    program7

--- B_PROG7.C
+++ B_PROG7.C
@@ -3,7 +3,10 @@ void main()
 {
  int n,i;
  scanf("%d",&n);
+ if(n<=0)
+  printf("Enter the positive no ");
+  else{
  for(i=0;i<n;i++)
-   printf("Hello \n");
+   printf("Hello \n");}
  getch();
 }
