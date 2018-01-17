e44f0998fd90acc6e24ae2865b0377f62c30b361
priyanksharmaa <35268463+priyanksharmaa@users.noreply.github.com>
2018-01-17 21:51:11 +0530

    prog15

---
+++ B_PROG15.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int n1,n2, i;
+ scanf("%d %d",&n1,&n2);
+ if(n1<=100000&& n2<=100000)
+ {
+ for(i=n1+1;i<n2;i++)
+  {
+   if(i%2==0)
+     printf(" %d ",i);
+  }
+ }
+ else
+   printf("one of the no is greater than 100000");
+ getch();
+}
